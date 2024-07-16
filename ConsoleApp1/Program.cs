5ffe7beb32845c1562d0dd773599b1d27b4be1fc
Peterson Gregorio Araujo <petersonaraujo70@gmail.com>
2024-07-16 15:09:45 -0300

    Estrutura while

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a senha: ");
+        string senha = Console.ReadLine();
+
+        while (senha != "2002")
+        {
+            Console.WriteLine("Senha inválida!");
+            Console.WriteLine("Digite novamente: ");
+            senha = Console.ReadLine();
+        }
+        Console.WriteLine("Acesso permitido!");
+    }
+}
---
+++ PostoCombustivel/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+        int combustivel;
+
+        Console.WriteLine("Digite o tipo de combustível que deseja abastecer (1. Álcool, 2.Gasolina ou 3.Diesel): ");
+        combustivel = int.Parse(Console.ReadLine());        
+
+        while (combustivel != 4) {
+            switch (combustivel)
+            {
+                case 1:
+                    Console.WriteLine("Álcool");
+                    alcool++;
+                    break;
+                case 2:
+                    Console.WriteLine("Gasolina");
+                    gasolina++;
+                    break;
+                case 3:
+                    Console.WriteLine("Diesel");
+                    diesel++;
+                    break;
+            }
+
+            if (combustivel >= 4) {
+                Console.WriteLine("Digite um código válido!");
+            }
+
+            Console.WriteLine("Digite o tipo de combustível que deseja abastecer (1. Álcool, 2.Gasolina ou 3.Diesel): ");
+            combustivel = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Muito obrigado!");
+        Console.WriteLine("Alcool: " + alcool);
+        Console.WriteLine("Gasolina: " + gasolina);
+        Console.WriteLine("Diesel: " + diesel);
+    }
+}
